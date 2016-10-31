e9d0cd5e33dca5deaff3e8270e2e6765c239c2ed
Putri <sangfiad@gmail.com>
2016-10-31 10:51:02 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/intent1/HasilActivity.java
@@ -2,12 +2,40 @@ package id.sch.smktelkom_mlg.intent1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class HasilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR, 0);
+
+        int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunLahir = yearNow - umur;
+
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + " Lahir pada tahun" + tahunLahir);
+
+        setTitle(" Hasil");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
